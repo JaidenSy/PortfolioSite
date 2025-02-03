183a18a356a2c9b461bb323089e54feb6c39e1cb
unknown <JaidenSy07@gmail.com>
2025-02-02 16:03:59 -0800

    connected backend with frontend

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello from Golang backend!")
-    })
+	r := mux.NewRouter()
 
-    fmt.Println("Server is running on http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+	r.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Golang backend!")
+	})
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("../frontend/build")))
+
+	// Enable CORS
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"http://localhost:3000"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", handlers.CORS(headersOk, originsOk, methodsOk)(r))
 }
